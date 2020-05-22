f75718c67c9cf668a0a16c12e7686562b635730e
plannoa <2heewoong@naver.com>
2020-05-22 19:20:01 +0900

    add timeblock in flownet 2.0

--- utils/tools.py
+++ utils/tools.py
@@ -1,6 +1,39 @@
+import time
+
 class StaticCenterCrop(object):
     def __init__(self, image_size, crop_size):
         self.th, self.tw = crop_size
         self.h, self.w = image_size
     def __call__(self, img):
         return img[(self.h-self.th)//2:(self.h+self.th)//2, (self.w-self.tw)//2:(self.w+self.tw)//2,:]
+
+
+class TimerBlock:
+    def __init__(self, title):
+        print(("{}".format(title)))
+
+    def __enter__(self):
+        self.start = time.clock()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.end = time.clock()
+        self.interval = self.end - self.start
+
+        if exc_type is not None:
+            self.log("Operation failed\n")
+        else:
+            self.log("Operation finished\n")
+
+    def log(self, string):
+        duration = time.clock() - self.start
+        units = 's'
+        if duration > 60:
+            duration = duration / 60.
+            units = 'm'
+        print(("  [{:.3f}{}] {}".format(duration, units, string)))
+
+    def log2file(self, fid, string):
+        fid = open(fid, 'a')
+        fid.write("%s\n" % (string))
+        fid.close()
