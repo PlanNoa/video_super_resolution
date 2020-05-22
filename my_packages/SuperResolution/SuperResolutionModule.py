fda1496f8af174411d33763d39b7133575e1aad6
plannoa <2heewoong@naver.com>
2020-05-22 19:19:32 +0900

    change projection init location

--- my_packages/SuperResolution/SuperResolutionModule.py
+++ my_packages/SuperResolution/SuperResolutionModule.py
@@ -1,18 +1,18 @@
 from torch.nn import Module
-import torch
-from torch.autograd import Variable
-from torch.autograd import gradcheck
-
 from ..DepthProjection.DepthProjectionModule import DepthProjectionModule
 from ..FlowProjection.FlowProjectionModule import FlowProjectionModule
-from .SuperResolutionLayer import SuperResolutionLayer
+
+# Super Resolution with depth map and optical flow for unnatural objects  in video super resolution
 
 class SuperResolutionModule(Module):
     def __init__(self):
         super(SuperResolutionModule, self).__init__()
+        self.FlowModule = FlowProjectionModule()
+        self.DepthModule = DepthProjectionModule()
 
-    def forward(self, input1, input2):
-        optical_flow = FlowProjectionModule(input1, input2)
-        depth_map = DepthProjectionModule(input1, input2)
-        concat = torch.cat(optical_flow, depth_map)
-        return SuperResolutionLayer.apply(input1, input2, concat)
+    def forward(self, input1, input2, input3):
+        optical_flow = [self.FlowModule(input1, input2),
+                        self.FlowModule(input2, input3)]
+        depth_map = [self.DepthModule(input1, input2),
+                     self.DepthModule(input2, input3)]
+        return
