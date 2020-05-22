d70f59cd9ce74a73fa584458ea3a8a7512712b8e
plannoa <2heewoong@naver.com>
2020-05-22 19:18:51 +0900

    change flownet arguments and cropper

--- my_packages/FlowProjection/FlowProjectionModule.py
+++ my_packages/FlowProjection/FlowProjectionModule.py
@@ -5,14 +5,25 @@ import torch
 import numpy as np
 
 class FlowProjectionModule(Module):
-    def __init__(self, image_size, render_size):
+    def __init__(self, image_size=None, render_size=None):
         super(FlowProjectionModule, self).__init__()
+        self.cropper = None
         self.net = FlowNet2().cuda()
-        self.cropper = StaticCenterCrop(image_size, render_size)
         self.dict = torch.load("pretrained/FlowNet2_checkpoint.pth.tar")
         self.net.load_state_dict(self.dict["state_dict"])
+        self.image_size = image_size
+        self.render_size = render_size
+        # if image_size != None or render_size != None:
+        #     self.cropper = StaticCenterCrop(image_size, render_size)
+        # else:
+        #     self.cropper = None
 
     def forward(self, input1, input2):
+        if self.cropper == None or self.image_size != input1.shape[:2] or \
+           self.render_size != [((input1.shape[0])//64 )*64, ((input1.shape[1])//64)*64]:
+            self.image_size = input1.shape[:2]
+            self.render_size = [((input1.shape[0])//64 )*64, ((input1.shape[1])//64)*64]
+            self.cropper = StaticCenterCrop(self.image_size, self.render_size)
         images = [input1, input2]
         images = list(map(self.cropper, images))
 
