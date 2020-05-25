f918a37bdaea605f95d61137c6a65a5579998382
plannoa <2heewoong@naver.com>
2020-05-25 09:18:08 +0900

    remove initialize_weight function

--- network/video_super_resolution.py
+++ network/video_super_resolution.py
@@ -11,16 +11,14 @@ class VSR(torch.nn.Module):
         self.Flow_loss = _Flow_loss()
         self.loss4object = _loss4object()
 
-    def _initialize_weights(self):
-        # init pretrained model
-        pass
-
-    def forward(self):
-        pass
+    def forward(self, input1, input2, input3, target, outputs):
+        output = self.model(input1, input2, input3)
+        loss = self.loss_calculate(output, target, outputs)
+        return self.model(input1, input2, input3), loss
 
     def loss_calculate(self, output, target, outputs):
         genSR_loss = self.SR_loss(output, target)
         objSR_loss = np.mean(self.SR_loss(i, j) for i, j in self.loss4object(output, target))
         Flow_loss = self.Flow_loss(outputs)
         objFlow_loss = self.Flow_loss(self.loss4object(outputs))
-        pass
+        return (genSR_loss + objSR_loss + Flow_loss + objFlow_loss)/4
