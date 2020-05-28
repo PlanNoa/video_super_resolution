7d20f5cfa4ba0f915a568ec4ab2eaeccb44f8af8
plannoa <2heewoong@naver.com>
2020-05-28 19:41:06 +0900

    make part of Modules and network clear

--- my_packages/SuperResolution/SuperResolutionModule.py
+++ my_packages/SuperResolution/SuperResolutionModule.py
@@ -1,27 +1,10 @@
 from torch.nn import Module
-from ..DepthProjection.DepthProjectionModule import DepthProjectionModule
-from ..FlowProjection.FlowProjectionModule import FlowProjectionModule
 
 # Super Resolution with depth map and optical flow for unnatural objects  in video super resolution
 
 class SuperResolutionModule(Module):
     def __init__(self):
         super(SuperResolutionModule, self).__init__()
-        self.FlowModule = FlowProjectionModule().eval()
-        self.DepthModule = DepthProjectionModule().eval()
 
-    def forward(self, input, estimated_image=None):
-        optical_flow = [self.FlowModule(input[0], input[1]),
-                        self.FlowModule(input[1], input[2])]
-        depth_map = [self.DepthModule(input[0], input[1]),
-                     self.DepthModule(input[1], input[2])]
-
-        if estimated_image:
-            # None 아닐때 optical flow, depth map과 그냥 concat
-            pass
-        else:
-            #None 일때 estimated t-1 자리에 VOS(estimated t-1, estimated t+1)
-            pass
-
-
+    def forward(self, input):
         return
--- network/video_super_resolution.py
+++ network/video_super_resolution.py
@@ -2,18 +2,47 @@ import torch
 import numpy as np
 from my_packages.SuperResolution import SuperResolutionModule
 from loss_function import _SR_loss, _Flow_loss, _loss4object
+from my_packages.DepthProjection.DepthProjectionModule import DepthProjectionModule
+from my_packages.FlowProjection.FlowProjectionModule import FlowProjectionModule
+from my_packages.VOSProjection.VOSProjectionModule import VOSProjectionModule
+from utils.tools import up_scaling
 
 class VSR(torch.nn.Module):
     def __init__(self):
         super(VSR, self).__init__()
         self.model = SuperResolutionModule()
+        self.FlowModule = FlowProjectionModule().eval()
+        self.DepthModule = DepthProjectionModule().eval()
+        self.VOSModule = VOSProjectionModule.eval()
         self.SR_loss = _SR_loss()
         self.Flow_loss = _Flow_loss()
         self.loss4object = _loss4object()
 
     def forward(self, data, target, high_frames, estimated_image, train=True):
-        output = self.model(data, estimated_image)
-        output = self.model([estimated_image, output, data])
+
+        optical_flow = [self.FlowModule(data[0], data[1]),
+                        self.FlowModule(data[1], data[2])]
+        depth_map = [self.DepthModule(data[0], data[1]),
+                     self.DepthModule(data[1], data[2])]
+        optical_flow = torch.tensor(list(map(up_scaling, optical_flow)))
+        depth_map = torch.tensor(list(map(up_scaling, depth_map)))
+        input = torch.cat((optical_flow[0], optical_flow[1],
+                          depth_map[0], depth_map[1],
+                          estimated_image), 0)
+        output = self.model(input)
+
+        data = [estimated_image, output, data[2]]
+        optical_flow = [self.FlowModule(data[0], data[1]),
+                        self.FlowModule(data[1], data[2])]
+        depth_map = [self.DepthModule(data[0], data[1]),
+                     self.DepthModule(data[1], data[2])]
+        VOSmask = self.VOSModule(data[0], data[1]) != 0
+        estimated_image = np.bitwise_and(input[1], VOSmask)
+        input = torch.cat((optical_flow[0], optical_flow[1],
+                          depth_map[0], depth_map[1],
+                          estimated_image))
+        output = self.model(input)
+
         high_frames[1] = output
         loss = self.loss_calculate(output, target, high_frames) if train else None
         return output, loss
