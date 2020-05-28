d5291bcdd77754bf37eacfa0d7238a460dbbb60f
Donggeun Yu <ddonggeunn@gmail.com>
2020-05-28 23:03:51 +0900

    Add SR

---
+++ my_packages/SuperResolution/blocks.py
@@ -0,0 +1,47 @@
+
+def ConvBlock(in_channels, out_channels, kernel_size, stride=1, dilation=1, bias=True, valid_padding=True, padding=0,\
+              act_type='relu', norm_type='bn', pad_type='zero', mode='CNA'):
+    assert (mode in ['CNA', 'NAC']), '[ERROR] Wrong mode in [%s]!'%sys.modules[__name__]
+
+    if valid_padding:
+        padding = get_valid_padding(kernel_size, dilation)
+    else:
+        pass
+    p = pad(pad_type, padding) if pad_type and pad_type != 'zero' else None
+    conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride=stride, padding=padding, dilation=dilation, bias=bias)
+
+    if mode == 'CNA':
+        act = activation(act_type) if act_type else None
+        n = norm(out_channels, norm_type) if norm_type else None
+        return sequential(p, conv, n, act)
+    elif mode == 'NAC':
+        act = activation(act_type, inplace=False) if act_type else None
+        n = norm(in_channels, norm_type) if norm_type else None
+        return sequential(n, act, p, conv)
+
+def DeconvBlock(in_channels, out_channels, kernel_size, stride=1, dilation=1, bias=True, padding=0, \
+                act_type='relu', norm_type='bn', pad_type='zero', mode='CNA'):
+    assert (mode in ['CNA', 'NAC']), '[ERROR] Wrong mode in [%s]!'%sys.modules[__name__]
+
+    p = pad(pad_type, padding) if pad_type and pad_type != 'zero' else None
+    deconv = nn.ConvTranspose2d(in_channels, out_channels, kernel_size, stride, padding, dilation=dilation, bias=bias)
+
+    if mode == 'CNA':
+        act = activation(act_type) if act_type else None
+        n = norm(out_channels, norm_type) if norm_type else None
+        return sequential(p, deconv, n, act)
+    elif mode == 'NAC':
+        act = activation(act_type, inplace=False) if act_type else None
+        n = norm(in_channels, norm_type) if norm_type else None
+        return sequential(n, act, p, deconv)
+
+class MeanShift(nn.Conv2d):
+    def __init__(self, rgb_mean, rgb_std, sign=-1):
+        super(MeanShift, self).__init__(3, 3, kernel_size=1)
+        std = torch.Tensor(rgb_std)
+        self.weight.data = torch.eye(3).view(3, 3, 1, 1)
+        self.weight.data.div_(std.view(3, 1, 1, 1))
+        self.bias.data = sign * 255. * torch.Tensor(rgb_mean)
+        self.bias.data.div_(std)
+        for p in self.parameters():
+            p.requires_grad = False
