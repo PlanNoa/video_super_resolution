85ca38d5ea470094949859267d9a0c4c2d06c1a3
plannoa <2heewoong@naver.com>
2020-05-29 20:19:53 +0900

    remove same function and import

--- utils/tools.py
+++ utils/tools.py
@@ -1,7 +1,22 @@
 import time, os, shutil, torch
 from PIL import Image
+import errno
+import hashlib
+import os
+import re
+import shutil
+import sys
+import tempfile
 import numpy as np
-
+import warnings
+import zipfile
+from tqdm import tqdm
+if sys.version_info[0] == 2:
+    from urlparse import urlparse
+    from urllib2 import urlopen  # noqa f811
+else:
+    from urllib.request import urlopen
+    from urllib.parse import urlparse  # noqa: F401
 
 class StaticCenterCrop(object):
     def __init__(self, image_size, crop_size):
@@ -9,7 +24,6 @@ class StaticCenterCrop(object):
         self.h, self.w = image_size
     def __call__(self, img):
         return img[(self.h-self.th)//2:(self.h+self.th)//2, (self.w-self.tw)//2:(self.w+self.tw)//2,:]
-
 
 class TimerBlock:
     def __init__(self, title):
@@ -80,3 +94,89 @@ def up_scaling(img):
     img = img.resize((int(x*2), int(y*2)))
     img = np.asarray(img)
     return img
+
+
+def load_state_dict_from_url(url, model_dir=None, map_location=None, progress=True, check_hash=False):
+
+    if os.getenv('TORCH_MODEL_ZOO'):
+        warnings.warn('TORCH_MODEL_ZOO is deprecated, please use env TORCH_HOME instead')
+
+    if model_dir is None:
+        torch_home = _get_torch_home()
+        model_dir = os.path.join(torch_home, 'checkpoints')
+
+    try:
+        os.makedirs(model_dir)
+    except OSError as e:
+        if e.errno == errno.EEXIST:
+            pass
+        else:
+            raise
+
+    parts = urlparse(url)
+    filename = os.path.basename(parts.path)
+    cached_file = os.path.join(model_dir, filename)
+    if not os.path.exists(cached_file):
+        sys.stderr.write('Downloading: "{}" to {}\n'.format(url, cached_file))
+        hash_prefix = re.compile(r'-([a-f0-9]*)\.').search(filename).group(1) if check_hash else None
+        download_url_to_file(url, cached_file, hash_prefix, progress=progress)
+
+    if zipfile.is_zipfile(cached_file):
+        with zipfile.ZipFile(cached_file) as cached_zipfile:
+            members = cached_zipfile.infolist()
+            if len(members) != 1:
+                raise RuntimeError('Only one file(not dir) is allowed in the zipfile')
+            cached_zipfile.extractall(model_dir)
+            extraced_name = members[0].filename
+            cached_file = os.path.join(model_dir, extraced_name)
+
+    return torch.load(cached_file, map_location=map_location)
+
+def _get_torch_home():
+    torch_home = os.path.expanduser(
+        os.getenv('TORCH_HOME',
+                  os.path.join(os.getenv('XDG_CACHE_HOME', '~/.cache'), 'torch')))
+    return torch_home
+
+def download_url_to_file(url, dst, hash_prefix=None, progress=True):
+
+    file_size = None
+
+    u = urlopen(url)
+    meta = u.info()
+    if hasattr(meta, 'getheaders'):
+        content_length = meta.getheaders("Content-Length")
+    else:
+        content_length = meta.get_all("Content-Length")
+    if content_length is not None and len(content_length) > 0:
+        file_size = int(content_length[0])
+
+    dst = os.path.expanduser(dst)
+    dst_dir = os.path.dirname(dst)
+    f = tempfile.NamedTemporaryFile(delete=False, dir=dst_dir)
+
+    try:
+        if hash_prefix is not None:
+            sha256 = hashlib.sha256()
+        with tqdm(total=file_size, disable=not progress,
+                  unit='B', unit_scale=True, unit_divisor=1024) as pbar:
+            while True:
+                buffer = u.read(8192)
+                if len(buffer) == 0:
+                    break
+                f.write(buffer)
+                if hash_prefix is not None:
+                    sha256.update(buffer)
+                pbar.update(len(buffer))
+
+        f.close()
+        if hash_prefix is not None:
+            digest = sha256.hexdigest()
+            if digest[:len(hash_prefix)] != hash_prefix:
+                raise RuntimeError('invalid hash value (expected "{}", got "{}")'
+                                   .format(hash_prefix, digest))
+        shutil.move(f.name, dst)
+    finally:
+        f.close()
+        if os.path.exists(f.name):
+            os.remove(f.name)
