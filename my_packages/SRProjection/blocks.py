abeb4825f0798b8e273b3ce20731cb21c0702f37
Donggeun Yu <ddonggeunn@gmail.com>
2020-05-29 13:49:13 +0900

    Update get_valid_padding, activation, pad, norm, sequential in blocks.py

--- my_packages/SRProjection/blocks.py
+++ my_packages/SRProjection/blocks.py
@@ -2,16 +2,18 @@ import sys
 import torch
 import torch.nn as nn
 
-def ConvBlock(in_channels, out_channels, kernel_size, stride=1, dilation=1, bias=True, valid_padding=True, padding=0,\
+
+def ConvBlock(in_channels, out_channels, kernel_size, stride=1, dilation=1, bias=True, valid_padding=True, padding=0, \
               act_type='relu', norm_type='bn', pad_type='zero', mode='CNA'):
-    assert (mode in ['CNA', 'NAC']), '[ERROR] Wrong mode in [%s]!'%sys.modules[__name__]
+    assert (mode in ['CNA', 'NAC']), '[ERROR] Wrong mode in [%s]!' % sys.modules[__name__]
 
     if valid_padding:
         padding = get_valid_padding(kernel_size, dilation)
     else:
         pass
     p = pad(pad_type, padding) if pad_type and pad_type != 'zero' else None
-    conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride=stride, padding=padding, dilation=dilation, bias=bias)
+    conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride=stride, padding=padding, dilation=dilation,
+                     bias=bias)
 
     if mode == 'CNA':
         act = activation(act_type) if act_type else None
@@ -22,9 +24,10 @@ def ConvBlock(in_channels, out_channels, kernel_size, stride=1, dilation=1, bias
         n = norm(in_channels, norm_type) if norm_type else None
         return sequential(n, act, p, conv)
 
+
 def DeconvBlock(in_channels, out_channels, kernel_size, stride=1, dilation=1, bias=True, padding=0, \
                 act_type='relu', norm_type='bn', pad_type='zero', mode='CNA'):
-    assert (mode in ['CNA', 'NAC']), '[ERROR] Wrong mode in [%s]!'%sys.modules[__name__]
+    assert (mode in ['CNA', 'NAC']), '[ERROR] Wrong mode in [%s]!' % sys.modules[__name__]
 
     p = pad(pad_type, padding) if pad_type and pad_type != 'zero' else None
     deconv = nn.ConvTranspose2d(in_channels, out_channels, kernel_size, stride, padding, dilation=dilation, bias=bias)
@@ -38,6 +41,7 @@ def DeconvBlock(in_channels, out_channels, kernel_size, stride=1, dilation=1, bi
         n = norm(in_channels, norm_type) if norm_type else None
         return sequential(n, act, p, deconv)
 
+
 class MeanShift(nn.Conv2d):
     def __init__(self, rgb_mean, rgb_std, sign=-1):
         super(MeanShift, self).__init__(3, 3, kernel_size=1)
@@ -48,3 +52,73 @@ class MeanShift(nn.Conv2d):
         self.bias.data.div_(std)
         for p in self.parameters():
             p.requires_grad = False
+
+
+def get_valid_padding(kernel_size, dilation):
+    """
+    Padding value to remain feature size.
+    """
+    kernel_size = kernel_size + (kernel_size - 1) * (dilation - 1)
+    padding = (kernel_size - 1) // 2
+    return padding
+
+
+def activation(act_type='relu', inplace=True, slope=0.2, n_prelu=1):
+    act_type = act_type.lower()
+    layer = None
+    if act_type == 'relu':
+        layer = nn.ReLU(inplace)
+    elif act_type == 'lrelu':
+        layer = nn.LeakyReLU(slope, inplace)
+    elif act_type == 'prelu':
+        layer = nn.PReLU(num_parameters=n_prelu, init=slope)
+    else:
+        raise NotImplementedError('[ERROR] Activation layer [%s] is not implemented!' % act_type)
+    return layer
+
+
+def norm(n_feature, norm_type='bn'):
+    norm_type = norm_type.lower()
+
+    layer = None
+
+    if norm_type == 'bn':
+
+        layer = nn.BatchNorm2d(n_feature)
+
+    else:
+
+        raise NotImplementedError('[ERROR] Normalization layer [%s] is not implemented!' % norm_type)
+
+    return layer
+
+
+def pad(pad_type, padding):
+    pad_type = pad_type.lower()
+    if padding == 0:
+        return None
+
+    layer = None
+    if pad_type == 'reflect':
+        layer = nn.ReflectionPad2d(padding)
+    elif pad_type == 'replicate':
+        layer = nn.ReplicationPad2d(padding)
+    else:
+        raise NotImplementedError('[ERROR] Padding layer [%s] is not implemented!' % pad_type)
+    return layer
+
+
+def sequential(*args):
+    if len(args) == 1:
+        if isinstance(args[0], OrderedDict):
+            raise NotImplementedError('[ERROR] %s.sequential() does not support OrderedDict' % sys.modules[__name__])
+        else:
+            return args[0]
+    modules = []
+    for module in args:
+        if isinstance(module, nn.Sequential):
+            for submodule in module:
+                modules.append(submodule)
+        elif isinstance(module, nn.Module):
+            modules.append(module)
+    return nn.Sequential(*modules)
