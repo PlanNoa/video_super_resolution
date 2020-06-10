20c520cb48c9cc395c66bf2eb2b628aaccf652d7
Donggeun Yu <ddonggeunn@gmail.com>
2020-06-10 11:12:30 +0900

    Update test.py

--- test.py
+++ test.py
@@ -2,6 +2,6 @@ import cv2
 import numpy as np
 from my_packages.DepthProjection.DepthProjectionModule import  DepthProjectionModule
 img = cv2.imread('video_super_resolution/demo.jpg')
-img = np.concatenate((img, img), 1)
+img = np.concatenate((img.T, img.T), 1)
 aa = DepthProjectionModule()
 aa.forward(img)
