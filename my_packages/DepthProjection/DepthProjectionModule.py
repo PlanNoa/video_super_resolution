60cb63dbe4323fd1516d90f5bb590bda7b05d136
Donggeun Yu <ddonggeunn@gmail.com>
2020-06-10 11:32:51 +0900

    Update DepthProjectionModule.py

--- my_packages/DepthProjection/DepthProjectionModule.py
+++ my_packages/DepthProjection/DepthProjectionModule.py
@@ -16,7 +16,7 @@ class DepthProjectionModule():
         print(input_0.size(), input_2.size())
         cur_input_0 = input_0
         cur_input_2 = input_2
-        cur_offset_input = torch.cat((cur_input_0, cur_input_2), dim=1)
+        cur_offset_input = torch.cat([cur_input_0, cur_input_2], dim=0)
         cur_filter_input = cur_offset_input
 
         print(cur_filter_input.size())
