6701c5a7944b6977910bd033612d8b0271465823
Donggeun Yu <ddonggeunn@gmail.com>
2020-06-10 10:13:52 +0900

    Update DepthProjectionModule.py

--- my_packages/DepthProjection/DepthProjectionModule.py
+++ my_packages/DepthProjection/DepthProjectionModule.py
@@ -4,7 +4,7 @@ from my_packages.DepthProjection import MegaDepth
 class DepthProjectionModule():
     def __init__(self):
         super(DepthProjectionModule, self).__init__()
-        self.depthNet=MegaDepth.__dict__['HourGlass']("./my_packages/DepthProjection/best_generalization_net_G.pth")
+        self.depthNet=MegaDepth.__dict__['HourGlass']("video_super_resolution/my_packages/DepthProjection/best_generalization_net_G.pth")
 
 
     def forward(self):
