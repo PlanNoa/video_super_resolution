9f1cc3764e2e7ea18c71e7d0c600498bd8705cce
Donggeun Yu <ddonggeunn@gmail.com>
2020-06-12 16:28:48 +0900

    Update test.py

--- test.py
+++ test.py
@@ -12,15 +12,14 @@ w_offset = random.choice(range(448 - input_frame_size[2] + 1))
 
 
 img = cv2.imread('video_super_resolution/demo.jpg')
+img cv2.resize(img, (input_frame_size))
 img = img[h_offset:h_offset + input_frame_size[1], w_offset: w_offset + input_frame_size[2], :]
 print(np.shape(img))
 
 img = np.transpose(img, (2,0,1))
 img = img.astype("float32")/ 255.0
-from PIL import Image
 
-img = Image.fromarray(img)
-img = img.resize((256, 448))
+
 img = torch.from_numpy(img)
 #img = torch.cat([img, img], dim=0)
 img = torch.stack([img, img], dim=0)
