7e9d4127c719759ff2281d5e89ddbcca623a9a40
plannoa <2heewoong@naver.com>
2020-07-15 14:14:57 +0900

    move load model code

--- my_packages/DepthProjection/models/HG_model.py
+++ my_packages/DepthProjection/models/HG_model.py
@@ -10,17 +10,13 @@ class HGModel(nn.Module):
         super(HGModel, self).__init__()
 
         model = pytorch_DIW_scratch.pytorch_DIW_scratch
-        if pretrained is None:
-            self.netG = model
-        else:
-            pretrained_dict = torch.load(pretrained)
+        pretrained_dict = torch.load(pretrained)
 
-            model_dict = model.state_dict()
-            pretrained_dict = {k[7:]: v for k, v in pretrained_dict.items()}
-            model_dict.update(pretrained_dict)
-            model.load_state_dict(model_dict)
-            self.netG = model
-
+        model_dict = model.state_dict()
+        pretrained_dict = {k[7:]: v for k, v in pretrained_dict.items()}
+        model_dict.update(pretrained_dict)
+        model.load_state_dict(model_dict)
+        self.netG = model
 
     def forward(self, input):
         input = Variable(input)
